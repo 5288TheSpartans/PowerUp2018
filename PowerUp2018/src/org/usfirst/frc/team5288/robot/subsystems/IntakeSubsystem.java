6a735f24b43fa43b73d2d8fb0d680e59c941e1c7
Tony <Tony@DESKTOP-9TOL2TK>
2018-02-17 13:33:13 -0500

    Added skeleton for intakeSubsystem

--- PowerUp2018/src/org/usfirst/frc/team5288/robot/subsystems/IntakeSubsystem.java
+++ PowerUp2018/src/org/usfirst/frc/team5288/robot/subsystems/IntakeSubsystem.java
@@ -1,6 +1,8 @@
 package org.usfirst.frc.team5288.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.VictorSP;
 
 /**
  *
@@ -9,10 +11,25 @@ public class IntakeSubsystem extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
+	private VictorSP lIntake = new VictorSP(RobotMap.TBD);
+	private VictorSP rIntake = new VictorSP(RobotMap.TBD);
+	public enum state{};
+	private state currentState;
+	
+	
 
     public void initDefaultCommand() {
         // Set the default command for a subsystem here.
         //setDefaultCommand(new MySpecialCommand());
+    }
+    
+    public void outputToIntake(double power) {
+    	lIntake.set(power);
+    	rIntake.set(power);
+    }
+    
+    public void setState(state a) {
+    	currentState = a;
     }
 }
 
