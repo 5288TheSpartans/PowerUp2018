cbf160ba15c3f0c578bfa9f8a331d92da4989006
Keyon <Keyon-Jerome.kjjawesomeness@gmail.com>
2018-02-20 16:19:40 -0500

    Added left side to left scale auto group.

---
+++ PowerUp2018/src/org/usfirst/frc/team5288/robot/autocommandGroups/autoLeftSidetoLeftScale.java
@@ -0,0 +1,40 @@
+package org.usfirst.frc.team5288.robot.autocommandGroups;
+
+import org.usfirst.frc.team5288.robot.autocommands.DriveStraightDistance;
+import org.usfirst.frc.team5288.robot.autocommands.SpotTurnDegrees;
+import org.usfirst.frc.team5288.robot.commands.intake.UnloadCube;
+import org.usfirst.frc.team5288.robot.commands.lift.LiftToHeight;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ *
+ */
+public class autoLeftSidetoLeftScale extends CommandGroup {
+    public autoLeftSidetoLeftScale() {
+        // Add Commands here:
+        // e.g. addSequential(new Command1());
+        //      addSequential(new Command2());
+        // these will run in order.
+    	addSequential(new DriveStraightDistance(320));
+    	addParallel(new LiftToHeight(100));
+    	addSequential(new SpotTurnDegrees(90));
+    	addSequential(new UnloadCube());
+    	addSequential(new LiftToHeight(0.0));
+    	addParallel(new SpotTurnDegrees(90));
+    	addSequential(new DriveStraightDistance(-88));
+    	addSequential(new SpotTurnDegrees(-90));
+    	
+        // To run multiple commands at the same time,
+        // use addParallel()
+        // e.g. addParallel(new Command1());
+        //      addSequential(new Command2());
+        // Command1 and Command2 will run in parallel.
+
+        // A command group will require all of the subsystems that each member
+        // would require.
+        // e.g. if Command1 requires chassis, and Command2 requires arm,
+        // a CommandGroup containing them would require both the chassis and the
+        // arm.
+    }
+}
