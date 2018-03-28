c00fb933d39f9ab4dfd313b89d524e49c2079ad5
Robotics <Robotics@10.228.43.200>
2018-03-28 16:30:08 -0400

    Overrides. Not all are finished.

---
+++ PowerUp2018/src/org/usfirst/frc/team5288/robot/commands/intake/HoldCubeOverride.java
@@ -0,0 +1,43 @@
+package org.usfirst.frc.team5288.robot.commands.intake;
+
+import org.usfirst.frc.team5288.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class HoldCubeOverride extends Command {
+
+    public HoldCubeOverride() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.intake.setOverride(true);
+    	Robot.intake.outputToIntake(0.0);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.intake.setOverride(false);
+    	Robot.intake.outputToIntake(-1.0);
+    }
+}
---
+++ PowerUp2018/src/org/usfirst/frc/team5288/robot/commands/intake/UnloadCubeOverride.java
@@ -0,0 +1,42 @@
+package org.usfirst.frc.team5288.robot.commands.intake;
+
+import org.usfirst.frc.team5288.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class UnloadCubeOverride extends Command {
+
+    public UnloadCubeOverride() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.intake);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.intake.setOverride(true);
+        Robot.intake.outputToIntake(0.5);
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    	Robot.intake.setOverride(false);
+    }
+}
